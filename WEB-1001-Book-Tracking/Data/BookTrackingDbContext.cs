f4a5e56f76eba2f8542af0cc9518331eb2ecb465
Tri Le <trile95.it@gmail.com>
2022-04-21 19:20:44 -0400

    add book tracking db context

---
+++ WEB-1001-Book-Tracking/Data/BookTrackingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WEB_1001_Book_Tracking.Models;
+
+namespace WEB_1001_Book_Tracking.Data
+{
+    public class BookTrackingDbContext : DbContext
+    {
+        public BookTrackingDbContext(DbContextOptions<BookTrackingDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryType> CategoryTypes { get; set; }
+    }
+}
